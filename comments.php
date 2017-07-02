f9ca4eb6ee4d83b54d4b407f284fce954530f62d
D-Bood <dbood2012@gmail.com>
2017-07-02 20:04:17 +0800

    personalized

--- comments.php
+++ comments.php
@@ -72,7 +72,7 @@ echo $commentClass;
 
     <?php if($this->allow('comment')): ?>
     <div id="<?php $this->respondId(); ?>" class="respond">
-        <div class="cancel-comment-reply"><span class="response"><?php _e('发表新评论'); ?></span><span class="cancel-reply"><?php $comments->cancelReply(); ?></span></div>
+        <div class="cancel-comment-reply"><span class="response"><?php _e('发表新评论 (*项为必填内容)'); ?></span><span class="cancel-reply"><?php $comments->cancelReply(); ?></span></div>
         <form method="post" action="<?php $this->commentUrl() ?>" id="comment-form" role="form">
             <?php if($this->user->hasLogin()): ?>
             <p style="padding-top:10px;"><?php _e('已登入: '); ?><a href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>. <a href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?> &raquo;</a></p>
@@ -87,13 +87,13 @@ echo $commentClass;
                 <?php else : ?>
                 <div id="author_info">
                 <?php endif ; ?>
-            <input type="text" name="author" maxlength="12" id="author" class="form-control" placeholder="<?php _e('称呼 *'); ?>" value="<?php $this->remember('author'); ?>">
-            <input type="email" name="mail" id="mail" class="form-control" placeholder="<?php _e('电子邮箱 *'); ?>" value="<?php $this->remember('mail'); ?>">
-            <input type="url" name="url" id="url" class="form-control" placeholder="<?php _e('网址(http://)'); ?>" value="<?php $this->remember('url'); ?>">
+            <input type="text" name="author" maxlength="12" id="author" class="form-control" placeholder="<?php _e('　称呼 *'); ?>" value="<?php $this->remember('author'); ?>">
+            <input type="email" name="mail" id="mail" class="form-control" placeholder="<?php _e('　电子邮箱 *'); ?>" value="<?php $this->remember('mail'); ?>">
+            <input type="url" name="url" id="url" class="form-control" placeholder="<?php _e('　网址(http://)'); ?>" value="<?php $this->remember('url'); ?>">
                </div>
                 <?php endif; ?>
 				
-            <textarea name="text" id="textarea" class="form-control" onkeydown="if(event.ctrlKey&&event.keyCode==13){document.getElementById('misubmit').click();return false};" placeholder="<?php _e('在这里输入你的评论(Ctrl/Cmd+Enter也可以提交)...'); ?>" required ><?php $this->remember('text',false); ?></textarea>
+            <textarea name="text" id="textarea" class="form-control" onkeydown="if(event.ctrlKey&&event.keyCode==13){document.getElementById('misubmit').click();return false};" placeholder="<?php _e('　在这里输入你的评论(Ctrl/Cmd+Enter也可以提交)...'); ?>" required ><?php $this->remember('text',false); ?></textarea>
             <button type="submit" class="submit" id="misubmit"><?php _e('提交评论'); ?></button>
             <?php $security = $this->widget('Widget_Security'); ?>
             <input type="hidden" name="_" value="<?php echo $security->getToken($this->request->getReferer())?>">
